f7717e6ca934438e508d9432d79e098e6dd10bce
fiorotticaio <caiofiorotti@gmail.com>
2023-10-01 10:32:38 -0300

    :hammer: adicionando arquivo jogada

--- compile.bat
+++ compile.bat
@@ -2,5 +2,6 @@
 nasm16 -f obj -o projeto\linec.obj -l projeto\linec.lst projeto\linec.asm
 nasm16 -f obj -o projeto\jogo.obj -l projeto\jogo.lst projeto\jogo.asm
 nasm16 -f obj -o projeto\desenho.obj -l projeto\desenho.lst projeto\desenho.asm
-freelink projeto\jogo projeto\linec projeto\desenho
+nasm16 -f obj -o projeto\jogada.obj -l projeto\jogada.lst projeto\jogada.asm
+freelink projeto\jogo projeto\linec projeto\desenho projeto\jogada
 projeto\jogo.exe
