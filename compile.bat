a765fbeaa5eea8b1d943e4f6f0abbfa9958ceae3
fiorotticaio <caiofiorotti@gmail.com>
2023-10-01 09:26:51 -0300

    :stethoscope: adicionando mais um arquivo no script

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
 @echo off
 nasm16 -f obj -o projeto\linec.obj -l projeto\linec.lst projeto\linec.asm
 nasm16 -f obj -o projeto\jogo.obj -l projeto\jogo.lst projeto\jogo.asm
-freelink projeto\jogo projeto\linec
+nasm16 -f obj -o projeto\desenho.obj -l projeto\desenho.lst projeto\desenho.asm
+freelink projeto\jogo projeto\linec projeto\desenho
 projeto\jogo.exe
