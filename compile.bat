63b5f816cacb8f80d9136927e82964ce052be7f4
fiorotticaio <caiofiorotti@gmail.com>
2023-10-08 19:42:48 -0300

    :seedling: atualizando scrpit de compilação

--- compile.bat
+++ compile.bat
@@ -3,5 +3,6 @@ nasm16 -f obj -o projeto\linec.obj -l projeto\linec.lst projeto\linec.asm
 nasm16 -f obj -o projeto\jogo.obj -l projeto\jogo.lst projeto\jogo.asm
 nasm16 -f obj -o projeto\desenho.obj -l projeto\desenho.lst projeto\desenho.asm
 nasm16 -f obj -o projeto\jogada.obj -l projeto\jogada.lst projeto\jogada.asm
-freelink projeto\jogo projeto\linec projeto\desenho projeto\jogada
+nasm16 -f obj -o projeto\erro.obj -l projeto\erro.lst projeto\erro.asm
+freelink projeto\jogo projeto\linec projeto\desenho projeto\jogada projeto\erro
 @REM projeto\jogo.exe
